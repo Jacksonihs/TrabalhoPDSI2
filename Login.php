a8e147c5d39f49d5543bdba1b39118f9f5656ed4
Jacksonihs <jacksonihs@hotmail.com>
2018-06-15 21:42:17 -0300

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,24 @@
+<head>
+        
+</head>
+    <body>
+
+
+        <div class="container">
+            <h2>Fazer Login</h2>
+            <form action="#">
+                <div class="form-group">
+                    <label for="email">E-mail:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Digite seu email" name="email">
+                </div>
+                              
+                <div class="form-group">
+                    <label for="senha">Senha:</label>
+                    <input type="password" class="form-control" id="senha" placeholder="Digite sua senha" name="senha">
+                </div>
+				
+            </form>
+        </div>
+
+    </body>
+</html>
